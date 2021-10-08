02a0cb32c04b47fd25a6a223ecb9e566c4a7b9d3
20046131 <69453644+20046131@users.noreply.github.com>
2021-10-08 09:48:48 +0800

    Completed all the UI elements

---
+++ LegoMobile/LegoMobile/MainMenuPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LegoMobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainMenuPage : ContentPage
+    {
+        public MainMenuPage()
+        {
+            InitializeComponent();
+        }
+        public async void LookUpSetPage()
+        {
+            NewFolder2.LookUpSet LookUpSet = new NewFolder2.LookUpSet();
+            await Navigation.PushModalAsync(LookUpSet);
+        }
+
+        private void LookUpSetButton_Clicked(object sender, EventArgs e)
+        {
+            LookUpSetPage();
+        }
+
+        public async void ViewAllCollections()
+        {
+            NewFolder1.ViewAllCollections viewCollections = new NewFolder1.ViewAllCollections();
+            await Navigation.PushModalAsync(viewCollections);
+
+        }
+        private void ViewCollectionsButton_Clicked(object sender, EventArgs e)
+        {
+            ViewAllCollections();
+        }
+
+        private void ViewWhishlistsButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LegoMobile/LegoMobile/NewFolder1/ViewAllCollections.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LegoMobile.NewFolder1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewAllCollections : ContentPage
+    {
+        public ViewAllCollections()
+        {
+            InitializeComponent();
+        }
+
+        public async void addCollection()
+        {
+            AddCollection addCollection = new AddCollection();
+            await Navigation.PushModalAsync(addCollection);
+
+        }
+        private void AddCollectionsButton_Clicked(object sender, EventArgs e)
+        {
+            addCollection();
+
+        }
+        public async void ViewCollection1()
+        {
+            ViewCollection viewCollection = new ViewCollection();
+            await Navigation.PushModalAsync(viewCollection);
+
+        }
+        private void ViewCollection1Button_Clicked(object sender, EventArgs e)
+        {
+            ViewCollection1();
+        }
+    }
+}
