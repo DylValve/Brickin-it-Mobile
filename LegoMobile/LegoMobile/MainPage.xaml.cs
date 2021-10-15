5ce642bdbe3e20ce38ae9a44c25d35cf816d8d24
Dylan Valverde <20047221@tafe.wa.edu.au>
2021-10-15 10:10:48 +0800

    User API

---
+++ LegoMobile/LegoMobile/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LegoMobile
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        bool LoggedIn = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            SwitchMenu();
+        }
+
+        public void SwitchMenu()
+        {
+            if (LoggedIn == true)
+            {
+                LoginButton.IsVisible = false;
+                SignUpButton.IsVisible = false;
+                LogOutButton.IsVisible = true;
+                MainMenuButton.IsVisible = true;
+            }
+            else
+            {
+                LoginButton.IsVisible = true;
+                SignUpButton.IsVisible = true;
+                LogOutButton.IsVisible = false;
+                MainMenuButton.IsVisible = false;
+            }
+        }
+
+        public async void LoginPage()
+        {
+            Users.Login getLoginPage = new Users.Login();
+            await Navigation.PushModalAsync(getLoginPage);
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            LoginPage();
+        }
+
+        public async void SignUpPage()
+        {
+            Users.SignUp getSignUpPage = new Users.SignUp();
+            await Navigation.PushModalAsync(getSignUpPage);
+        }
+        private void SignUpButton_Clicked(object sender, EventArgs e)
+        {
+            SignUpPage();
+        }
+
+        public async void MainMenuPage()
+        {
+            MainMenuPage MainMenu = new MainMenuPage();
+            await Navigation.PushModalAsync(MainMenu);
+        }
+
+        private void MainMenu_Clicked(object sender, EventArgs e)
+        {
+            if (LoggedIn == true)
+            {
+                MainMenuPage();
+            }
+            else
+            {
+                LoggedIn = false;
+                Console.WriteLine("Entry Denied");
+            }
+        }
+
+        private void LogOutButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LegoMobile/LegoMobile/Users/Login.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LegoMobile.Users
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            string UsernameAPI = "test";
+            string PasswordAPI = "test";
+            string Token = "test";
+            string NewToken = "NewToken";
+            string EnteredUsername = UsernameEntry.Text;
+            string EnteredPassword = PasswordEntry.Text;
+
+            if (EnteredUsername == UsernameAPI)
+            {
+                if (EnteredPassword == PasswordAPI)
+                {
+                    Console.WriteLine("Login");
+                    Token = NewToken;
+                    if (Token == NewToken)
+                    {
+                        MainPage();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Password");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid User");
+            }
+        }
+
+        public async void MainPage()
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
