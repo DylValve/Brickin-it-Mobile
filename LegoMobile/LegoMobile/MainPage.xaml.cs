0a921dd7f1657d53a0816e34b38077ab4a8c2da0
DylValve <69453621+DylValve@users.noreply.github.com>
2021-11-27 16:05:29 +0800

    Update MainPage.xaml.cs

--- LegoMobile/LegoMobile/MainPage.xaml.cs
+++ LegoMobile/LegoMobile/MainPage.xaml.cs
@@ -10,17 +10,18 @@ namespace LegoMobile
 {
     public partial class MainPage : ContentPage
     {
-        APIService API = ((App)Application.Current).API;
+        APIService API = ((App)Application.Current).API; // Reference to the API requests that can be called
 
         public MainPage()
         {
             InitializeComponent();
 
-            SwitchMenu();
+            SwitchMenu(); // triggers the switch menu function
         }
 
         public void SwitchMenu()
         {
+            // changes the buttons that appear on the main menu
             if (API.loggedIn == true)
             {
                 LoginButton.IsVisible = false;
@@ -39,33 +40,39 @@ namespace LegoMobile
 
         public async void LoginPage()
         {
+            // navigates to the login page
             Users.Login getLoginPage = new Users.Login();
             await Navigation.PushModalAsync(getLoginPage);
         }
 
         private void LoginButton_Clicked(object sender, EventArgs e)
         {
+            // triggers login page function
             LoginPage();
         }
 
         public async void SignUpPage()
         {
+            // navigates to the sign up page
             Users.SignUp getSignUpPage = new Users.SignUp();
             await Navigation.PushModalAsync(getSignUpPage);
         }
         private void SignUpButton_Clicked(object sender, EventArgs e)
         {
+            // triggers the sign up page funtion
             SignUpPage();
         }
 
         public async void MainMenuPage()
         {
+            // navigates to the main menu page
             MainMenuPage MainMenu = new MainMenuPage();
             await Navigation.PushModalAsync(MainMenu);
         }
 
         private void MainMenu_Clicked(object sender, EventArgs e)
         {
+            // triggers the main menu page function after checking if a user is logged in
             if (API.loggedIn == true)
             {
                 MainMenuPage();
@@ -78,8 +85,9 @@ namespace LegoMobile
 
         private async void LogOutButton_Clicked(object sender, EventArgs e)
         {
+            // logs the user out
             bool success = await API.LogoutRequest();
-            if (success)
+            if (success) // triggers the switch menu function
             {
                 SwitchMenu();
             }
@@ -87,6 +95,7 @@ namespace LegoMobile
 
         protected override void OnAppearing()
         {
+            // triggers the switch menu function
             SwitchMenu();
 
             base.OnAppearing();
