15829d5b86c015ebb5cc5e042a2e4d8bfa758ce4
DylValve <69453621+DylValve@users.noreply.github.com>
2021-11-27 16:08:33 +0800

    Update MainMenuPage.xaml.cs

--- LegoMobile/LegoMobile/MainMenuPage.xaml.cs
+++ LegoMobile/LegoMobile/MainMenuPage.xaml.cs
@@ -18,32 +18,38 @@ namespace LegoMobile
         }
         public async void LookUpSetPage()
         {
+            // navigates to the look up page
             Sets.LookUpSet lookUpSet = new Sets.LookUpSet();
             await Navigation.PushModalAsync(lookUpSet);
         }
 
         private void LookUpSetButton_Clicked(object sender, EventArgs e)
         {
+            // triggers the look up set page function
             LookUpSetPage();
         }
 
         public async void ViewAllCollections()
         {
+            // navigates to the view collections page
             Collections.ViewAllCollections viewCollections = new Collections.ViewAllCollections();
             await Navigation.PushModalAsync(viewCollections);
         }
         private void ViewCollectionsButton_Clicked(object sender, EventArgs e)
         {
+            // triggers the view collections function
             ViewAllCollections();
         }
 
         private void BarcodeButton_Clicked(object sender, EventArgs e)
         {
+            // triggers the view scan page function
             ScanPage();
         }
 
         public async void ScanPage()
         {
+            // navigates to the scan page
             Sets.Set set = new Sets.Set();
             ScanPage scanPage = new ScanPage(set);
             await Navigation.PushModalAsync(scanPage);
@@ -51,6 +57,7 @@ namespace LegoMobile
 
         private async void backArrow_Clicked(object sender, EventArgs e)
         {
+            // goes back to the previous page
             await Navigation.PopModalAsync();
         }
     }
